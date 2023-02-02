c063c1d07241dfaecccaa40dee936e143b1bfda2
dev2Cett <desenvolvedor2@cett.org.br>
2023-02-02 10:10:22 -0300

    fix bug tamanho do link

--- app/sistema/models/atividade.py
+++ app/sistema/models/atividade.py
@@ -9,7 +9,7 @@ from .departamento import Departamento
 class Atividade(models.Model):
     id = models.AutoField(primary_key=True)
     descricao = models.CharField(null = True, blank=True, max_length=300)
-    linkDocumentos = models.CharField(null = True, blank=True, max_length=300)
+    linkDocumentos = models.CharField(null = True, blank=True, max_length=5000)
     status = models.CharField(null = True, blank=True, max_length=50)
     acao = models.ForeignKey(Acao, on_delete=models.SET_NULL, null=True, blank=True)
     evento = models.ForeignKey(DpEvento, on_delete=models.SET_NULL, null=True, blank= True)
