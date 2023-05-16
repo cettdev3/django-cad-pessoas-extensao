6e735f6690f0d61a7036f3f3051861348b63427d
dev2Cett <desenvolvedor2@cett.org.br>
2023-05-16 14:08:20 -0300

    fix membro da equipe de excução nao aparecendo no update de atividade

--- app/sistema/views/siteMembroExecucaoViews.py
+++ app/sistema/views/siteMembroExecucaoViews.py
@@ -131,13 +131,12 @@ def eliminarMembroExecucao(request,codigo):
 @login_required(login_url='/auth-user/login-user')
 def membrosExecucaoSelect(request):
     data = {}
+    print("print dentro do select de membro de execução: ",request.GET)
     if request.GET.get('membro_execucao_id'):
         data['membro_execucao_id'] = int(request.GET.get('membro_execucao_id'))
     acao_id = request.GET.get('acao_id')
     evento_id = request.GET.get('evento_id') if request.GET.get('evento_id') else request.GET.get('dp_evento_id')
-    if acao_id:
-        data["membrosExecucao"] = MembroExecucao.objects.filter(acao__id=acao_id)
-    elif evento_id:
+    if evento_id:
         data["membrosExecucao"] = MembroExecucao.objects.filter(evento__id=evento_id)
     else:
         data["membrosExecucao"] = MembroExecucao.objects.all()
