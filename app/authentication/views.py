0320b01f4fcefae4bda08b2da7fae023a9fe2747
dev2Cett <desenvolvedor2@cett.org.br>
2023-08-09 15:12:51 -0300

    Controle para usuarios da escola nao acessarem a home

--- app/authentication/views.py
+++ app/authentication/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from sistema.models import Pessoas
 
 def login_user(request):
     if request.method == "POST":
@@ -9,7 +10,10 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect("/home")
+            if request.user.pessoa.instituicao == Pessoas.INSTITUICAO_CETT:
+                return redirect("/home")
+            else:   
+                return redirect("/cotec-projeto-index")
         else:
             messages.error(request, ("Houve um erro verifique suas credenciais e tente novamente"))
             return render(request, "authenticate/login.html", {})  
