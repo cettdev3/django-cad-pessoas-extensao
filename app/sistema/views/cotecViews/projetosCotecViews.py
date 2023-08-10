d2105ebebe4ebf1c8e3067fe91b969fb39930ec9
dev2Cett <desenvolvedor2@cett.org.br>
2023-08-10 16:56:02 -0300

    Rota para envio de email

--- app/sistema/views/cotecViews/projetosCotecViews.py
+++ app/sistema/views/cotecViews/projetosCotecViews.py
@@ -497,12 +497,3 @@ def createProjetoFromProposta(request, pk):
         proposta_projeto.status = "aprovada"
         proposta_projeto.save()
     return JsonResponse({"message": "Projeto criado com sucesso!"})
-
-
-def sendEmail(request):
-    proposta_projeto = PropostaProjeto.objects.all().first()
-    try:
-        success = PropostaSubmetidaEmail(proposta_projeto).send()
-    except Exception as e:
-        print(e)
-    return JsonResponse({"message": "Email enviado com sucesso!"})
